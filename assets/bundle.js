58c1741dea135934539412192585623de8f538e2
roy <jnmcluo@163.com>
2019-05-30 17:49:35 +0800

    init

---
+++ assets/bundle.js
@@ -0,0 +1,22 @@
+// icons
+import "./lib/@coreui/icons/css/coreui-icons.min.css"; 
+import "./lib/font-awesome/css/font-awesome.min.css";
+import "./lib/simple-line-icons/css/simple-line-icons.css";
+// lib styles
+import "./lib/pace-progress/css/pace.min.css"
+// main styles
+import "./css/style.min.css";
+
+//import "@coreui/coreui";
+//import "@coreui/icons/css/";
+// Setup jquery
+import $ from "jquery";
+window.$ = window.jQuery = $;
+
+// Import other scripts
+import "./lib/popper.js/js/popper.min.js";
+import "./lib/bootstrap/js/bootstrap.min.js";
+import "./lib/pace-progress/js/pace.min.js";
+import "./lib/perfect-scrollbar/js/perfect-scrollbar.min.js";
+import "./lib/@coreui/coreui/js/coreui.min.js"
+ 
